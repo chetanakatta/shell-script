3a588922280d3a97ba5e6a5be8a3925d8c81a113
chetana <chetana@gmail.com>
2024-07-08 21:06:18 +0530

    sample script

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+USERID=$(id -u) #if 0 super user
+if [ $USERID -ne 0 ]
+   then 
+       echo "please run this script with root access"
+fi       
 dnf install mysql -y
+echo "is script proceeding?"
