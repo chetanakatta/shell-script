8559004b4626457b6778ba1ff7b0780596dfdf68
chetana <chetana@gmail.com>
2024-07-08 23:12:22 +0530

    sample script

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE() {
     if [ $1 -ne 0 ]
@@ -20,10 +23,8 @@ else
 fi
 
 dnf install mysql -y &>>$LOGFILE
-
 VALIDATE $? "installing my sql"
 
 dnf install git -y &>>$LOGFILE
-
 VALIDATE $? "installing git"
 
