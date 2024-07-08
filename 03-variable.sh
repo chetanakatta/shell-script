2cbbfa19c3f8da0b98cd87ac91e8e7237b4fb5bf
chetana <chetana@gmail.com>
2024-07-08 19:03:57 +0530

    sample script

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,7 +3,7 @@
 PERSON1=Sachin
 PERSON2=Rahul
 
-echo "$PERSON1:: Hello PERSON2, How are you?"
-echo "$PERSON2:: Hello PERSON1, I'm fine. How are you?"
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I'm fine. How are you?"
 echo "$PERSON1:: I'm fine too. How is your work?"
 echo "$PERSON2:: It's good"
