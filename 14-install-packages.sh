8432b7bf80e9505daeb28d421705af03a08ef3c5
chetana <chetana@gmail.com>
2024-07-09 00:06:12 +0530

    sample script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERId=$(id -u)
+USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
@@ -8,6 +8,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
@@ -17,30 +18,25 @@ VALIDATE(){
         echo -e "$2...$G SUCCESS $N"
     fi
 }
-F
-if [ $USERID -ne 0 ]
- then 
-   echo "run with root access"
-   exit 1
- else 
-   echo "you are super user"
-fi   
 
-echo "All packages: $@"
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
 for i in $@
- do 
-   echo "packages to install: $i"
-    dnf list installed $i &>>$LOGFILE # to check installed in package
+do
+    echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done
 
-  if [ $? -eq 0 ]
-   then 
-    echo -e "$i already installed..$Y skipping $N"
-   else
-    dnf install $i -y &>>$LOGFILE
-    VALIDATE $? "installation of $i"
-  fi  
- done
-
-
-  
