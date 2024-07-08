4a1a91141551f78f751e7bbeb4fb99297bf41c01
chetana <chetana@gmail.com>
2024-07-08 22:55:43 +0530

    sample script

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,12 @@
 USERID=$(id -u)
 
 VALIDATE() {
-    echo "Exit status: $1"
-    echo "What are you doing: $2"
+    if [ $1 -ne 0 ]
+      then
+          echo "$2...failure"
+      else  
+          echo "$2...success"
+    fi      
 }
 
 if [ $USERID -ne 0 ]
