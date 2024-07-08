1a351ac7ba6d5dcf34fd17139cdda25c4bdf944f
chetana <chetana@gmail.com>
2024-07-08 20:43:40 +0530

    sample script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "all variables: $@"
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "current working directory: $pwd"
+echo "home directory of current user: $HOME"
+echo "which user is running the script: $USER"
+echo "host name: $HOSTNAME"
+echo "process id of the current shell script: $$"
+
+
