8bcc02572cf780148c692d490a929627fe2cea37
chetana <chetana@gmail.com>
2024-07-08 15:20:27 +0530

    sample script

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hi suresh. how are you?"
+echo "suresh:: hello remesh. i'm fine. how are you?"
+echo "remesh:: i am fine too. how is your work?"
+echo "suresh:: not bad."
