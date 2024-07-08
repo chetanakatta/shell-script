6df4ff6affc5e089d871af865caeb48bc6550f68
chetana <chetana@gmail.com>
2024-07-08 23:50:57 +0530

    sample script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 USERId=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
 if [ $USERID -ne 0 ]
  then 
    echo "run with root access"
@@ -14,8 +27,15 @@ echo "All packages: $@"
 for i in $@
  do 
    echo "packages to install: $i"
-    dnf list installed $i # to chech installed in packages
+    dnf list installed $i &>>$LOGFILE # to check installed in package
+
+  if [$? -eq 0]
+   then 
+    echo "$i already installed..skipping"
+   else
+    echo "$i not installed..need to install"
+  fi  
  done
- 
-      
+
+
   
