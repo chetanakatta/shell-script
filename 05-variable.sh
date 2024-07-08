cb610ff083c1898a6caf13c0b8f1937ad437d4d3
chetana <chetana@gmail.com>
2024-07-08 19:16:09 +0530

    sample script

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Please enter username ::"
-read USERNAME
+read -s USERNAME
 echo "Please enter Password ::"
-read PASSWORD
+read -s PASSWORD
 
 echo "Username is : $USERNAME, Password is : $PASSWORD"
