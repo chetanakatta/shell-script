423ed1f5c85282aeb3b777e7766792a605627970
chetana <chetana@gmail.com>
2024-07-08 21:01:52 +0530

    sample script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NUMBER=$1
-if [ $NUMBER -gt 10 ] #gt-->greater than
+if [ $NUMBER -gt 10 ] #-gt-->greater than; -lt-->less than; -eq-->equal to; -le-->less than equal to
    then 
        echo "given no $NUMBER is greater than 10"
    else 
