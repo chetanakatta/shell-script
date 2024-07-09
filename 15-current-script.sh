92c13ada326b2badd52f2fb38885c79325d6cd0b
chetana <chetana@gmail.com>
2024-07-09 12:15:16 +0530

    testing

---
+++ 15-current-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+echo "Before calling the script, course: $COURSE"
+echo "Process id of current shell script: $$"
+#./16-other-script.sh
+echo "After calling the script, cours: $COURSE"
