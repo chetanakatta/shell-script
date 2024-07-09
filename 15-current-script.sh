63e788e951011b57be011c51e4e46237ce3ff745
chetana <chetana@gmail.com>
2024-07-09 12:40:35 +0530

    script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,7 @@ COURSE="Devops from current script"
 echo "Before calling the script, course: $COURSE"
 echo "Process id of current shell script: $$"
 #1st method
-#./16-other-scriptsh -->>pid of 2 are diff & variable values after calling the script is diff
+#./16-other-script.sh -->>pid of 2 are diff & variable values after calling the script is diff
 #2nd method
-source ./16-other-scriptsh #-->> pid of 2 are same & variable values after calling the script is same
+source ./16-other-script.sh #-->> pid of 2 are same & variable values after calling the script is same
 echo "After calling the script, course: $COURSE"
