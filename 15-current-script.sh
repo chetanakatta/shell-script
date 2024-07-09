1e3e2ee695329fb398d2b7ae1d114d3a1feb154d
chetana <chetana@gmail.com>
2024-07-09 12:25:21 +0530

    script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COURSE="Devops from current script"
+
 echo "Before calling the script, course: $COURSE"
 echo "Process id of current shell script: $$"
 source ./16-other-script.sh
--- 16-other-script.sh
+++ 16-other-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 COURSE="Devops from other script"
+
 echo "variable value from the other script: $COURSE"
 echo "Process id of other script: $$"
